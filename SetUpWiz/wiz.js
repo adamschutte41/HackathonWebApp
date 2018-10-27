19628b41d36c952e54857e79775978a8d323b09d
ernchi <khoo.ernchi@gmail.com>
2018-10-27 15:41:10 -0400

    Set up wiz functions

--- SetUpWiz/wiz.js
+++ SetUpWiz/wiz.js
@@ -3,11 +3,20 @@ function getCourseInfo() {
   var startTime = document.getElementById("startTime").value;
   var endTime = document.getElementById("endTime").value;
   var day = document.getElementById("day").value;
-  var course = [courseName, startTime, endTime, day];
+  var repeat = document.getElementById("repeat").value;
+  var courseSchedule = [courseName, startTime, endTime, day, repeat];
 }
 
 function getWorkInfo() {
   var workStartTime = document.getElementById("workStartTime").value;
   var workEndTime = document.getElementById("workEndTime").value;
-  var day = document.getElementsById("day");
+  var workDay = document.getElementsById("day").value;
+  var repeat = document.getElementById("repeat").value;
+  var workSchedule = [workStartTime, workEndTime, workDay, repeat];
+}
+
+function getTask() {
+  var taskName = document.getElementById("taskName").value;
+  var taskTimeEst = document.getElementById("taskTimeEst").value;
+  var dailyTask = [taskName, taskTimeEst];
 }
