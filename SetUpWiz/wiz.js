3f8f96e95cd75f86232b92ae57744dc62804c4a5
ernchi <khoo.ernchi@gmail.com>
2018-10-27 15:22:48 -0400

    Update set up wizard

---
+++ SetUpWiz/wiz.js
@@ -0,0 +1,13 @@
+function getCourseInfo() {
+  var courseName = document.getElementById("courseName").value;
+  var startTime = document.getElementById("startTime").value;
+  var endTime = document.getElementById("endTime").value;
+  var day = document.getElementById("day").value;
+  var course = [courseName, startTime, endTime, day];
+}
+
+function getWorkInfo() {
+  var workStartTime = document.getElementById("workStartTime").value;
+  var workEndTime = document.getElementById("workEndTime").value;
+  var day = document.getElementsById("day");
+}
